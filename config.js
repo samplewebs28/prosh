08e0bcfe355e55cbc8341e4449d5f0fe1a6d54e4
samplewebs28 <bindhusreedasari@gmail.com>
2026-03-27 14:49:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  web3formsKey: 'e58cf668-2132-4a07-a017-7a6cfa37d9ac',
+  jsonbinApiKey: '$2a$10$dZKWzf6BM52dGwe4IewlR.qX4NuDytwvBsM3YiyntZ5AMi9895D5C',
+  jsonbinBinId: '69c62becb7ec241ddcaace75'
+};
